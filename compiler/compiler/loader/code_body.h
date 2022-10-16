aa4e2d7e82244114e95fdf2f207e1e8775ecd67a
ttehbx222 <54137798+ttehbx222@users.noreply.github.com>
2022-10-16 11:08:32 +0200

    Remake code_body

--- compiler/compiler/loader/code_body.h
+++ compiler/compiler/loader/code_body.h
@@ -19,3 +19,110 @@
 #include <string>
 #include <map>
 #include <vector>
+
+namespace nylium {
+	namespace codebody{
+
+		class Type;
+
+		enum class Castable {
+			CAST_DIRECT,
+			CAST_POSSIBLE,
+			CAST_IMPOSSIBLE
+		};
+
+		enum Visibility {
+			PRIVATE = 0x0,
+			PROTECTED = 0x1,
+			PUBLIC = 0x2
+		};
+
+		enum class CodeType {
+			SCOPE,
+			OPERATION
+		};
+
+		enum class OperationType {
+			FUNCTION_CALL,
+			ASSIGN,
+			MEMBER_CALL
+		};
+
+		enum class DeclarationType {
+			TYPE,
+			FIELD,
+			FUNCTION,
+			REFERENCE
+		};
+
+		enum CodeObjectType {
+			CODE_LINE = 0x1,
+			VALUE_HOLDER = 0x2
+		};
+
+		class CodeObject {
+
+		};
+
+		class CodeLine : public CodeObject {
+
+		};
+
+		class ValueHolder : public CodeObject {
+
+		};
+
+		class Scope : public CodeLine {
+
+		};
+
+		class Operation : public CodeLine, public ValueHolder {
+
+		};
+
+		class FunctionCall : public Operation {
+
+		};
+
+		class AssignOperation : public Operation {
+
+		};
+
+		class MemberCall : public Operation {
+
+		};
+
+		class Declaration : public ValueHolder {
+
+		};
+
+		class TypeDeclaration : public Declaration {
+
+		};
+
+		class FieldDeclaration : public Declaration {
+
+		};
+
+		class FunctionDeclaration : public Declaration {
+
+		};
+
+		class ReferenceDeclaration : public Declaration {
+
+		};
+
+		class UnresolvedDeclaration : public Declaration {
+
+		};
+
+		class Specification {
+
+		};
+
+		class Type {
+
+		};
+
+	}
+}
